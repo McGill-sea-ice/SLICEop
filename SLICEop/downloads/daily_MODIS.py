d85c0d9ad7ee93e29b42e36a2a2dcde92df7391d
jk-rieck <26458242+jk-rieck@users.noreply.github.com>
2025-04-30 16:07:46 -0400

    added comments to daily download scripts

--- SLICEop/downloads/daily_MODIS.py
+++ SLICEop/downloads/daily_MODIS.py
@@ -3,19 +3,28 @@ import datetime
 import requests
 
 now = datetime.datetime.now()
+# specify directory to store the downloaded image
 out_dir = "/aos/home/jrieck/src/SLICEop/SLICEop/downloads/MODIS/"
 
+# extract year, month and day from datetime.datetime.now()
 year = f"{now.year:04d}"
 month = f"{now.month:02d}"
 day = f"{(now.day - 1):02d}"
 
 print("Downloading MODIS satellite image for " + year + "-" + month + "-" + day)
 
-url = str("https://wvs.earthdata.nasa.gov/api/v1/snapshot?REQUEST=GetSnapshot&"
-          + "TIME=" + year +"-" + month + "-" + day + "T00:00:00Z&BBOX=45.1855,-74.2417,45.8297,-73.261&"
-          + "CRS=EPSG:4326&LAYERS=MODIS_Terra_CorrectedReflectance_TrueColor,Coastlines_15m&WRAP=day,"
-          + "x&FORMAT=image/jpeg&WIDTH=893&HEIGHT=586&colormaps=,&ts=1744122633289")
+# define url to download image
+# to define different limits of the shown image, change
+# BBOX=45.1855,-74.2417,45.8297,-73.261
+url = str("https://wvs.earthdata.nasa.gov/api/v1/"
+          + "snapshot?REQUEST=GetSnapshot&"
+          + "TIME=" + year +"-" + month + "-" + day + "T00:00:00Z"
+          + "&BBOX=45.1855,-74.2417,45.8297,-73.261&"
+          + "CRS=EPSG:4326&LAYERS=MODIS_Terra_CorrectedReflectance_TrueColor,"
+          + "Coastlines_15m&WRAP=day,x&FORMAT=image/jpeg"
+          + "&WIDTH=893&HEIGHT=586&colormaps=,&ts=1744122633289")
 
+# download image and save to out_dir
 with open(out_dir + 'worldview.jpg', 'wb') as handle:
     response = requests.get(url, stream=True)
     if not response.ok:
--- SLICEop/downloads/daily_Twater.py
+++ SLICEop/downloads/daily_Twater.py
@@ -7,72 +7,123 @@ import datetime
 import csv
 import sys
 
+# path to SLICEop
 path = "/aos/home/jrieck/src/SLICEop/SLICEop/"
+thermistor_path = "/storage/thermistor/"
 
-# function to read data from thermistor (works for files newer than Longueuil.dat4860.dat)
-def read_thermistor_new(path):
-    tmp = pd.read_csv(path, sep=",",  header=None,
-                      quoting=csv.QUOTE_ALL).to_xarray().rename({0: "Date", 2: "T"}).drop_vars((1, 3, 4, 5))
+
+# function to read data from thermistor (works for files newer than
+# Longueuil.dat4860.dat, before that the format was different)
+def read_thermistor_new(inputfile):
+    # read thermistor data and convert to xarray, define dimension names
+    tmp = pd.read_csv(inputfile, sep=",",  header=None,
+        quoting=csv.QUOTE_ALL).to_xarray().rename({0: "Date",
+            2: "T"}).drop_vars((1, 3, 4, 5))
+    # set Date as coordinate and add it as an index
     tmp = tmp.set_coords("Date")
     tmp["Date"] = pd.DatetimeIndex(tmp['Date'].values)
+    # replace bad values with NaN
     tmpT = tmp.T.data
-    replace = [np.nan if tmpT[i] == 'Bad' else float(tmpT[i]) for i in range(0, len(tmpT))]
+    replace = [np.nan if tmpT[i] == 'Bad' else float(tmpT[i])
+               for i in range(0, len(tmpT))]
     tmp["T"] = ("Date", replace)
+    # clean up dataset an return
     return tmp.drop_vars("index").sortby("Date")
 
-# load existing data
+# load existing temperature record
 ds_in = xr.open_dataset(path + "downloads/Twater/Twater_Longueuil_updated.nc")
-# get lowest index to add to data
+# get lowest suffix for temperature data to be added to ds_in
 with open(path + "downloads/Twater/next.i", "rb") as f:
     lowest_i = int(f.read())
 f.close()
-print("Adding new data to Twater_Longueuil_updated.nc, starting at Longueuil.dat" + str(lowest_i) + ".dat")
-# make sure to only include complete days, i.e. up until yesterday, in the daily averages to append to the existing datset
+print("Adding new data to Twater_Longueuil_updated.nc,"
+      + "starting at Longueuil.dat" + str(lowest_i) + ".dat")
+# make sure to only include complete days, i.e. up until yesterday, in the
+# daily averages to append to the existing datset
 now = np.datetime64(datetime.datetime.now())
-yesterday = np.datetime64(np.datetime_as_string(now - np.timedelta64(1, "D"), unit='D') + "T23:59:59")
+yesterday = np.datetime64(np.datetime_as_string(now - np.timedelta64(1, "D"),
+                          unit='D') + "T23:59:59")
+# the last day in ds_in + 1 is the first should be the first day that is added
 firstday = ds_in.Date.isel(Date=-1) + np.timedelta64(1, "D")
+# compute difference between yesterday and first day to be added
+# if this is 0, there is no new day to be added
 tds = float((yesterday - firstday).values) / 1e9
 # load data from thermistor
 i = lowest_i
 if tds > 0:
-    da_update = read_thermistor_new("/storage/thermistor/Longueuil.dat" + str(i) + ".dat")
+    # read new temperature data from thermistor
+    da_update = read_thermistor_new(thermistor_path + "Longueuil.dat"
+                                    + str(i) + ".dat")
     i += 1
-    while os.path.isfile("/storage/thermistor/Longueuil.dat" + str(i) + ".dat"):
+    # add new data until the most recent file
+    while os.path.isfile(thermistor_path + "Longueuil.dat" + str(i) + ".dat"):
         try:
-            tmp = read_thermistor_new("/storage/thermistor/Longueuil.dat" + str(i) + ".dat")
+            tmp = read_thermistor_new(thermistor_path + "Longueuil.dat"
+                                      + str(i) + ".dat")
         except:
+            # if there is an error of some sort, we will add a NaN to the
+            # time series
             tmp["Date"] = tmp["Date"] + np.timedelta64(1, "h")
             tmp["T"] = np.nan
+        # do not add data from today
         if tmp.Date.isel(Date=-1) <= yesterday:
-            da_update = xr.concat((da_update, tmp), dim="Date")
+            da_update = xr.concat((da_update, tmp), dim="Date",
+                                  coords="minimal", compat="override")
             i += 1
         else:
             break
+    # save the last suffix added to da_update so that we can restart from there
+    # the day after
     with open(path + "downloads/Twater/next.i", "w") as f:
         f.write(str(i))
     f.close()
 else:
-    print("No full day of temperature data available since " + str(firstday.values))
+    print("No full day of temperature data available since "
+          + str(firstday.values))
     sys.exit("Exiting daily_Twater.py")
 
+# drop duplicate values on time axis
+da_update = da_update.sortby("Date").drop_duplicates(dim="Date")
 # run a quick smoothing to exclude outliers before computing the daily average
 update_smooth = da_update.copy()
-dTdt_ok = np.abs(da_update["T"].ffill(dim="Date").differentiate("Date", datetime_unit="h")) < 0.1
-update_smooth["T"] = da_update.T.where(dTdt_ok & (da_update.T < 30.), other=np.nan)
-# compute daily average from hourly data
-updated = xr.merge([ds_in, update_smooth.sortby("Date").resample(Date="1D").mean(dim="Date")]).compute()
+# if the temperature changes more than 1 degree per hour, is larger than
+# 30 degrees or deviates more than 3 degrees from the climatological average
+# of its daym it is considered an outlier and we replace it with an
+# interpolated value
+dTdt_ok = np.abs(da_update["T"].ffill(dim="Date").differentiate("Date",
+             datetime_unit="h")) < 1.0
+update_smooth["T"] = da_update.T.where(dTdt_ok & (da_update.T < 30.),
+                                       other=np.nan)
+tmp = update_smooth.copy()
+tmp = tmp.assign_coords(year_month_day = tmp.Date.dt.strftime("%Y-%m-%d"))
+deviation = (tmp.groupby("year_month_day") -
+             tmp.groupby("year_month_day").mean("Date"))
+update_smooth["T"] = update_smooth.T.where(deviation.T < 3.,
+                         other=np.nan).interpolate_na(dim="Date")
+# compute daily average from hourly data and concatenate them to the
+# existing data in ds_in
+updated = xr.concat([ds_in,
+    update_smooth.sortby("Date").resample(Date="1D").mean(dim="Date")],
+    dim="Date", compat="override", coords="minimal")
+# make sure Date is in ascending order and there are no duplicate dates
+updated = updated.sortby("Date").drop_duplicates(dim="Date").compute()
 # save updated dataset
 ds_in.close()
-updated.to_netcdf(path + "downloads/Twater/Twater_Longueuil_updated.nc", mode="w")
+updated.to_netcdf(path + "downloads/Twater/Twater_Longueuil_updated.nc",
+                  mode="w")
 # save info on whether data was updated
 with open(path + "downloads/Twater/updated", "w") as f:
     f.write(str("True"))
 f.close()
-# check if St. Lawrence is already frozen
+# check if St. Lawrence is already frozen, if it is below 0.75 deg. Celsius
+# we consider it frozen
 if updated.T.isel(Date=-1).values < 0.75:
     with open(path + "auto/frozen", "r") as f:
         frozen = f.read()
     f.close()
+    # if the river is frozen based on yesterdays water temperature, but it was
+    # not frozen the day before, we consider yesterday as the freeze-up date and
+    # write that date to a file
     if frozen == "False":
         with open(path + "auto/frozenDate", "w") as f:
             f.write(str(updated.Date.isel(Date=-1).values)[0:10])
