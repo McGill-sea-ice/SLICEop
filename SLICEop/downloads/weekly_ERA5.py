ef3eaec8b0e82505512f95adee468a9186fce6bc
jk-rieck <26458242+jk-rieck@users.noreply.github.com>
2025-06-11 08:41:39 -0400

    added possibility for partial downloads in test case in weekly_ERA5.py

--- SLICEop/downloads/weekly_ERA5.py
+++ SLICEop/downloads/weekly_ERA5.py
@@ -81,6 +81,41 @@ def download_era5(var, month, year, output_dir, lats, lons):
             os.remove(filename)
     return
 
+    def download_era5_test(var, month, year, max_day, output_dir, lats, lons):
+    filename = output_dir + "ERA5_" + str(year) + month + "_" + var + ".partial.grib"
+    print("Downloading " + var + " for " + str(year) + month + " from ERA5")
+    try:
+        client = cdsapi.Client(retry_max=5)
+        client.retrieve(
+            'reanalysis-era5-single-levels',
+            {
+                'product_type': ['reanalysis'],
+                'data_format': 'grib',
+                'download_format': 'unarchived',
+                'variable': [var],
+                'area': [lats[1], lons[0], lats[0], lons[1]],
+                'time': [
+                    '00:00','01:00', '02:00','03:00',
+                    '04:00','05:00', '06:00','07:00',
+                    '08:00','09:00', '10:00','11:00',
+                    '12:00','13:00', '14:00','15:00',
+                    '16:00','17:00', '18:00','19:00',
+                    '20:00','21:00', '22:00','23:00',
+                ],
+                'day': [f"{np.arange(1, max_day+1)[i]:02d}" for i in range(0, max_day)],
+                # API ignores cases where there are less than 31 days
+                'month': [month],
+                'year': [year]
+            },
+            filename)
+    except Exception as e:
+        print(e)
+        # Delete the partially downloaded file.
+        if os.path.isfile(filename):
+            os.remove(filename)
+    return
+
+
 # define the variables and the respective months to download as well as the
 # desired region
 variables = ['2m_temperature', 'snowfall', 'total_cloud_cover']
@@ -88,89 +123,175 @@ months = ['12', '11', '09']
 lats = np.array([43.25, 46.00])
 lons = np.array([-77.25, -73.25])
 
-# depending on the month try to download ERA5 if it should be available
-if month == "09":
-    # do not replace SEAS5.1 cloud cover with ERA5 before the new SEAS5.1
-    # forecast is downloaded on the 7th
-    if day > 6:
-        try:
-            download_era5(variables[2], months[2], year, out_dir + "ERA5/", lats, lons)
-        except:
-            print("ERA5 " + variables[2] + " not downloaded")
-elif month == "10":
-    # only try to update SEAS5.1 cloud cover with ERA5 before the whole month
-    # of Sept. cloud cover is downloaded from ERA5 on the 7th
-    if day < 7:
-        try:
-            download_era5(variables[2], months[2], year, out_dir + "ERA5/", lats, lons)
-        except:
-       	    print("ERA5 " + variables[2] + " not downloaded")
+if os.environ["TEST"]=="True":
+    if month == "09":
+        if max_day > 0:
+            try:
+                download_era5(variables[2], months[2], year, max_day, out_dir + "ERA5/", lats, lons)
+            except:
+                print("ERA5 " + variables[2] + " not downloaded")
+    elif month == "10":
+        if day < 7:
+            if max_day > 0:
+                max_day = 30
+            else:
+                max_day = 30 + max_day
+            try:
+                download_era5(variables[2], months[2], year, max_day, out_dir + "ERA5/", lats, lons)
+            except:
+                print("ERA5 " + variables[2] + " not downloaded")
+        else:
+            try:
+                os.remove(local_path + "downloads/ERA5/ERA5_"
+                          + year + months[2] + "_" + variables[2] +  ".partial.grib")
+            except:
+                pass
+    elif month == "11":
+        if max_day > 0:
+            try:
+                download_era5(variables[1], months[1], year, max_day, out_dir + "ERA5/", lats, lons)
+            except:
+                print("ERA5 " + variables[1] + " not downloaded")
+    elif month == "12":
+        if day < 7:
+            if max_day > 0:
+                max_day1 = 30
+                try:
+                    download_era5(variables[1], months[1], year, max_day1, out_dir + "ERA5/", lats, lons)
+                    download_era5(variables[0], months[0], year, max_day, out_dir + "ERA5/", lats, lons)
+                except:
+                    print("ERA5 " + variables[1] + " and " + variables[0] + " not downloaded")
+            else:
+                max_day1 = 30 + max_day
+                try:
+                    download_era5(variables[1], months[1], year, max_day1, out_dir + "ERA5/", lats, lons)
+                except:
+                    print("ERA5 " + variables[1] + " and " + variables[0] + " not downloaded")
+        else:
+            try:
+                os.remove(local_path + "downloads/ERA5/ERA5_"
+                          + year + months[1] + "_" + variables[1] +  ".partial.grib")
+            except:
+                pass
+            try:
+                download_era5(variables[0], months[0], year, max_day, out_dir + "ERA5/", lats, lons)
+            except:
+                print("ERA5 " + variables[0] + " not downloaded")
+    elif month == "01":
+        year = str(int(year) - 1)
+        if day < 7:
+            if max_day > 0:
+                max_day = 31
+            else:
+                max_day = 31 + max_day
+            try:
+                download_era5(variables[0], months[0], year, max_day, out_dir + "ERA5/", lats, lons)
+            except:
+                print("ERA5 " + variables[0] + " not downloaded")
+        else:
+            try:
+                os.remove(local_path + "downloads/ERA5/ERA5_"
+                          + year + months[0] + "_" + variables[0] +  ".partial.grib")
+            except:
+                pass
     else:
-        try:
-            os.remove(path + "downloads/ERA5/ERA5_"
-                      + year + months[2] + "_" + variables[2] +  ".partial.grib")
-        except:
-            pass
-elif month == "11":
-    # do not replace SEAS5.1 snowfall with ERA5 before the new SEAS5.1
-    # forecast is downloaded on the 7th
-    if day > 6:
-        try:
-            download_era5(variables[1], months[1], year, out_dir + "ERA5/", lats, lons)
-        except:
-            print("ERA5 " + variables[1] + " not downloaded")
-elif month == "12":
-    # only try to update SEAS5.1 2m temperature with ERA5 before the whole
-    # month of Dec. 2m temperature is downloaded from ERA5 on the 7th and
-    # do not replace SEAS5.1 2m temperature with ERA5 before the new SEAS5.1
-    # forecast is downloaded on the 7th
-    if day < 7:
-        try:
-            download_era5(variables[1], months[1], year, out_dir + "ERA5/", lats, lons)
-        except:
-            print("ERA5 " + variables[1] + " not downloaded")
-    else:
-        try:
-            os.remove(path + "downloads/ERA5/ERA5_"
-                      + year + months[1] + "_" + variables[1] +  ".partial.grib")
-        except:
-            pass
-        try:
-            download_era5(variables[0], months[0], year, out_dir + "ERA5/", lats, lons)
-        except:
-            print("ERA5 " + variables[0] + " not downloaded")
-elif month == "01":
-    # even in `year+1` we need the predictors from `year`
-    year = str(int(year) - 1)
-    # only try to update SEAS5.1 2m temperature with ERA5 before the whole
-    # month of Dec. 2m temperature is downloaded from ERA5 on the 7th
-    if day < 7:
-        try:
-            download_era5(variables[0], months[0], year, out_dir + "ERA5/", lats, lons)
-        except:
-            print("ERA5 " + variables[0] + " not downloaded")
-    else:
-        try:
-            os.remove(path + "downloads/ERA5/ERA5_"
-                      + year + months[0] + "_" + variables[0] +  ".partial.grib")
-        except:
-            pass
+        with open(local_path + "auto/frozen", "r") as f:
+            frozen = f.read()
+        f.close()
+        if frozen == "True":
+            frozen = True
+        else:
+            frozen = False
+        if frozen:
+            frozen = False
+            with open(local_path + "test/auto/frozen", "w") as f:
+                f.write(str(frozen))
+            f.close()
+        print("No additional data found to improve the forecast ")
 else:
-    # if in May or June, make sure to reset the variable `frozen` to `False`
-    # in preparation for the next winter's forecast
-    with open(path + "auto/frozen", "r") as f:
-        frozen = f.read()
-    f.close()
-    if frozen == "True":
-        frozen = True
+    # depending on the month try to download ERA5 if it should be available
+    if month == "09":
+        # do not replace SEAS5.1 cloud cover with ERA5 before the new SEAS5.1
+        # forecast is downloaded on the 7th
+        if day > 6:
+            try:
+                download_era5(variables[2], months[2], year, out_dir + "ERA5/", lats, lons)
+            except:
+                print("ERA5 " + variables[2] + " not downloaded")
+    elif month == "10":
+        # only try to update SEAS5.1 cloud cover with ERA5 before the whole month
+        # of Sept. cloud cover is downloaded from ERA5 on the 7th
+        if day < 7:
+            try:
+                download_era5(variables[2], months[2], year, out_dir + "ERA5/", lats, lons)
+            except:
+       	        print("ERA5 " + variables[2] + " not downloaded")
+        else:
+            try:
+                os.remove(path + "downloads/ERA5/ERA5_"
+                          + year + months[2] + "_" + variables[2] +  ".partial.grib")
+            except:
+                pass
+    elif month == "11":
+        # do not replace SEAS5.1 snowfall with ERA5 before the new SEAS5.1
+        # forecast is downloaded on the 7th
+        if day > 6:
+            try:
+                download_era5(variables[1], months[1], year, out_dir + "ERA5/", lats, lons)
+            except:
+                print("ERA5 " + variables[1] + " not downloaded")
+    elif month == "12":
+        # only try to update SEAS5.1 2m temperature with ERA5 before the whole
+        # month of Dec. 2m temperature is downloaded from ERA5 on the 7th and
+        # do not replace SEAS5.1 2m temperature with ERA5 before the new SEAS5.1
+        # forecast is downloaded on the 7th
+        if day < 7:
+            try:
+                download_era5(variables[1], months[1], year, out_dir + "ERA5/", lats, lons)
+            except:
+                print("ERA5 " + variables[1] + " not downloaded")
+        else:
+            try:
+                os.remove(path + "downloads/ERA5/ERA5_"
+                        + year + months[1] + "_" + variables[1] +  ".partial.grib")
+            except:
+                pass
+            try:
+                download_era5(variables[0], months[0], year, out_dir + "ERA5/", lats, lons)
+            except:
+                print("ERA5 " + variables[0] + " not downloaded")
+    elif month == "01":
+        # even in `year+1` we need the predictors from `year`
+        year = str(int(year) - 1)
+        # only try to update SEAS5.1 2m temperature with ERA5 before the whole
+        # month of Dec. 2m temperature is downloaded from ERA5 on the 7th
+        if day < 7:
+            try:
+                download_era5(variables[0], months[0], year, out_dir + "ERA5/", lats, lons)
+            except:
+                print("ERA5 " + variables[0] + " not downloaded")
+        else:
+            try:
+                os.remove(path + "downloads/ERA5/ERA5_"
+                        + year + months[0] + "_" + variables[0] +  ".partial.grib")
+            except:
+                pass
     else:
-        frozen = False
-    if frozen:
-        frozen = False
+        # if in May or June, make sure to reset the variable `frozen` to `False`
+        # in preparation for the next winter's forecast
+        with open(path + "auto/frozen", "r") as f:
+            frozen = f.read()
+        f.close()
+        if frozen == "True":
+            frozen = True
+        else:
+            frozen = False
+        if frozen:
+            frozen = False
         with open(path + "auto/frozen", "w") as f:
             f.write(str(frozen))
         f.close()
-    print("No additional data found to improve the forecast ")
+        print("No additional data found to improve the forecast ")
 
 # save info on whether data was updated
 with open(path + "downloads/updatew", "w") as f:
