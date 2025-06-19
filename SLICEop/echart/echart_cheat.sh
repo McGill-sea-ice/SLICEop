8ad657e6c0ac20fece62f4e71a3782f423526a36
jk-rieck <26458242+jk-rieck@users.noreply.github.com>
2025-06-19 14:54:20 -0400

    added javascript and html files needed to create echarts

---
+++ SLICEop/echart/echart_cheat.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# to avoid needing to set up a html server etc. to get the data loaded into
+# the echart, we just convert the data from json into javascript-compliant
+# files so we load it into the echart as scripts not as data
+{ echo -n 'datain='; cat ${path}/echart/sliceop_data.json; } > ${path}/echart/sliceop_data.js
+
+{ echo -n 'color='; cat ${path}/echart/colormap.json; } > ${path}/echart/colormap.js
+
+{ echo -n 'fud='; cat ${path}/echart/fuds.json; } > ${path}/echart/fuds.js
