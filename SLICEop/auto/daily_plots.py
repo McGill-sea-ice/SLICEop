ee5297b4936040c6661ecb9be541992c0e3c0148
jk-rieck <26458242+jk-rieck@users.noreply.github.com>
2025-07-03 13:04:19 -0400

    updated daily plot of Twater

--- SLICEop/auto/daily_plots.py
+++ SLICEop/auto/daily_plots.py
@@ -50,20 +50,36 @@ twu = xr.open_dataset(
 twp = xr.open_dataset(
     path + "/prepro/Twater_Longueuil_preprocessed.nc"
     )
+# load observed freeze-up dates
+FUD = xr.open_dataset(
+    path + "/prepro/FUD_preprocessed.nc"
+    )
+# compute mean observed freeze-up
+fud_clim = FUD.FUDoy.mean().values
+if fud_clim > 365:
+    fud_clim -= 365
+    climfrozenDate = str(datetime.datetime.strptime("2002 " + str(int(fud_clim)), "%Y %j"))
+else:
+    climfrozenDate = str(datetime.datetime.strptime("2001 " + str(int(fud_clim)), "%Y %j"))
 # remove mean offset from current season's data
 tw = (twu.T - twp.T_winter_offset.mean().values)
 # compute climatological cycle
 tw_c = twp.T_no_offset.groupby("Date.dayofyear").mean("Date").values[0:-1]
-# offest the climatology so that it runs from
+# offest the climatology so that it runs from July 1 tp June 30
 tw_c1 = tw_c[182::]
 tw_c2 = tw_c[0:182]
 tw_clim = np.hstack([tw_c1, tw_c2])
+# use a random, non-leap year to create a time axis for the plot. only the
+# month and day will be used later, the year is unimportant
 climtime = pd.date_range(
     start=datetime.datetime.strptime("2001 182", "%Y %j"),
     end=datetime.datetime.strptime("2002 181", "%Y %j"),
     freq="1D"
     )
 y = int(year)
+# define the date range to extract from the time series
+# start one day later in leap years, i.e. keeping the delta t to the last
+# day of the year the same
 if y % 4 == 0 and (y % 100 != 0 or y % 400 == 0):
     ctime = pd.date_range(
         start=datetime.datetime.strptime(year + " " + str(183), "%Y %j"),
@@ -78,12 +94,17 @@ else:
                                        "%Y %j"),
         freq="1D"
         )
+# remove the mean offset to make the
+# recent data comparable to preprocessed data from past seasons
 tw = (twu.T.sel(Date=slice(str(ctime[0]), str(ctime[-1])))
       - twp.T_winter_offset.mean().values)
+# set negatives to 0
 tw[tw < 0] = 0
+# add missing values for future dates
 missing = 365 - len(tw)
 tw = list(list(tw) + [np.nan] * missing)
 
+# if river is already frozen, get the date of freeze-up
 with open(path + "/auto/frozen", "r") as f:
     frozen = f.read()
 f.close()
@@ -95,38 +116,53 @@ if frozen == "True":
 else:
     frozen = False
 
+# define x-axis limits of dummy time axis
 xax_min = np.datetime64("2001-07-01")
 xax_max = np.datetime64("2002-06-30")
+# define x ticks, one every month
 xticks = np.unique(pd.date_range(start=xax_min, freq="MS", periods=13).round("1D"))
 xticks_diff = xticks[1] - xticks[0]
+# define the month and day for each x-tick to construct the labels later 
 xtickmonths = [xticks[i].astype("datetime64[M]").astype(int) % 12 + 1 for i in range(0, len(xticks))]
 xtickdays = [(xticks[i].astype('datetime64[D]') - xticks[i].astype('datetime64[M]') + 1).astype(int) for i in range(0, len(xticks))]
-
+# define min and maz of y-axis
 yax_min = -1
 yax_max = 28
 
+# loop over either french or english
 for l in ["fr_CA", "en_CA"]:
     try:
         locale.setlocale(locale.LC_TIME, l)
     except:
         pass
+    # set xticklabels after locale has been set
     xticklabels = [datetime.datetime.strptime("2000 " + f"{xtickmonths[i]:02d}" + " "
                                               + f"{xtickdays[i]:02d}", '%Y %m %d').strftime('%d %b')
                                               + "      " for i in range(0, len(xticks))]
+    # define labels depending on language
     if l == 'fr_CA':
         climlabel = "climatologie"
         waterlabel = "observations"
         frozenlabel = "date de gel observée"
+        climfrozenlabel = "date de gel moyen"
         title = "température de l'eau à Longueuil"
     elif l == 'en_CA':
         climlabel = "climatology"
         waterlabel = "observations"
         frozenlabel = "observed freeze-up"
+        climfrozenlabel = "climatological freeze-up"
         title = "Water temperature at Longueuil"
+    # setup figure
     fig = plt.figure(figsize=(5, 4))
     ax1 = fig.add_subplot()
+    # plot climatology and recent observations
     ax1.plot(climtime, tw_clim, color="gray", ls="--", label=climlabel)
     ax1.plot(climtime, tw, color="steelblue", lw=2, label=waterlabel)
+    # add climatological freeze-up date
+    ax1.vlines(np.datetime64(climfrozenDate), yax_min, yax_max, ls="--", lw=0.5, color="dimgray")
+    ax1.text(np.datetime64(climfrozenDate) - (xticks_diff/3), (yax_min + yax_max)/1.33, climfrozenlabel,
+             ha="left", va="center", rotation=90, color="dimgray", fontsize=7)
+    # if river is frozen, add the freeze-up date as vertical line
     if frozen:
         ax1.vlines(np.datetime64(frozenDate), yax_min, yax_max, ls="--", color="firebrick")
         ax1.text(np.datetime64(frozenDate) + (xticks_diff/10), (yax_min + yax_max)/2, frozenlabel,
